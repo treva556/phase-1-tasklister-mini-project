cd1ff7bde53c13af7bdd1695a1baf811228352ba
treva556 <marktreva92@gmail.com>
2022-12-06 12:39:54 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    addTask(e.target.addTask.value);
+    form.reset();
+  })
 });
+
+function addTask(e){
+  var li = document.createElement('li')
+  li.textContent = `${e} `
+  const parent = document.getElementById('tasks')
+  parent.appendChild(li)
+  li.appendChild(btn)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
